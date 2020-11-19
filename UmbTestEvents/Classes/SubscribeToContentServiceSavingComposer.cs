ab203baa3f8cc21bcfd199246bfa89b6d5cdc6fd
Brianmanden <brianmanden@gmail.com>
2020-11-19 20:34:19 +0100

    Add hooking into Saving and Publishing event.

---
+++ UmbTestEvents/Classes/SubscribeToContentServiceSavingComposer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Events;
+using Umbraco.Core.Services;
+using Umbraco.Core.Services.Implement;
+
+
+namespace UmbTestEvents.Classes
+{
+    public class SubscribeToContentServiceSavingComposer :IUserComposer
+    {
+        public void Compose(Composition composition) {
+            composition.Components().Append<SubscribeToContentServiceSavingComponent>();
+        }
+    }
+
+    public class SubscribeToContentServiceSavingComponent : IComponent{
+        public void Initialize() {
+            ContentService.Saving += ContentService_Saving;
+        }
+
+        public void Terminate()
+        {
+            ContentService.Saving -= ContentService_Saving;
+        }
+
+        public void ContentService_Saving(IContentService sender, ContentSavingEventArgs e) {
+            System.Diagnostics.Debug.WriteLine("Saving & publishing");
+            foreach (var content in e.SavedEntities.Where(c => c.ContentType.Alias.InvariantEquals("MyContentType"))){
+                System.Diagnostics.Debug.WriteLine("Content found");
+            }
+        }
+    }
+}
