9c9e2e840a064a42af70f5711f954f503c6c9c6f
Brianmanden <brianmanden@gmail.com>
2020-11-19 21:35:28 +0100

    Add insert RichTextEditor value into MongoDb at Save & Publish.

--- UmbTestEvents/Classes/SubscribeToContentServiceSavingComposer.cs
+++ UmbTestEvents/Classes/SubscribeToContentServiceSavingComposer.cs
@@ -5,6 +5,8 @@ using Umbraco.Core.Composing;
 using Umbraco.Core.Events;
 using Umbraco.Core.Services;
 using Umbraco.Core.Services.Implement;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 
 namespace UmbTestEvents.Classes
@@ -17,6 +19,9 @@ namespace UmbTestEvents.Classes
     }
 
     public class SubscribeToContentServiceSavingComponent : IComponent{
+
+        private MongoClient client = new MongoClient("mongodb+srv://BrianUser:TheBrianUserPassword@cluster0.xh1tn.mongodb.net/BrianDatabaseTest?retryWrites=true&w=majority");
+
         public void Initialize() {
             ContentService.Saving += ContentService_Saving;
         }
@@ -30,6 +35,13 @@ namespace UmbTestEvents.Classes
             System.Diagnostics.Debug.WriteLine("Saving & publishing");
             foreach (var content in e.SavedEntities.Where(c => c.ContentType.Alias.InvariantEquals("MyContentType"))){
                 System.Diagnostics.Debug.WriteLine("Content found");
+                var umbContentValue = content.GetValue("rTEtest");
+                System.Diagnostics.Debug.WriteLine(umbContentValue);
+
+                var database = client.GetDatabase("BrianDatabaseTest");
+                var collection = database.GetCollection<BsonDocument>("BrianCollectionTest");
+                var document = new BsonDocument { { "theValue", umbContentValue.ToString() } };
+                collection.InsertOne(document);
             }
         }
     }
